813c9f14e106e869c9b3da9e68af4cd4749e1487
Kirill Asyamolov <kirilla@playtika.com>
2020-02-12 22:11:21 +0300

    remove viewer part

--- src/Game.hx
+++ src/Game.hx
@@ -33,17 +33,12 @@ class Game {
     #if js
     private var canvas(default, null):CanvasElement;
     private var ctx(default, null):CanvasRenderingContext2D;
-    private var select:SelectElement;
-    private var range:InputElement;
-
-    private var animationKeys:Array<Int>;
 
     private var oldTimestamp:Float = 0;
     private var delta:Float = 0;
     private var iFrame:Int = 0;
     #end
 
-    private var clientState:ClientState;
     private var gameInfo:GameInfo;
     private var info:MapDrawingInfo;
     private var topTile:Int3;
@@ -51,13 +46,9 @@ class Game {
 
     private var animFrame:Int = 0;
 
-    private var anim:Animation;
-    private var group:Int = 0;
-
     public function new() {
         var mapName:String = "Vial of Life.h3m";
 
-        clientState = new ClientState();
         gameInfo = new GameInfo();
         topTile = new Int3(14, 76, 0);
         info = new MapDrawingInfo(topTile, [[[]]], new Rect(0, 0, 594, 546));
@@ -92,28 +83,6 @@ class Game {
         mapService.loadMapHeaderByName(mapName);
         mapService.loadMapByName(mapName);
         #end
-/*
-        var defaultFilename:String = "zm196z.def";
-
-        #if js
-        initControls();
-
-        // load binnary models
-        FileCache.instance.initGraphicsAsync().then(function(files:Array<String>) {
-            return files.filter(function(filename:String) {
-                return filename.indexOf(".def") > -1;
-            });
-        }).then(function(defFiles:Array<String>) {
-            initSelect(defFiles);
-            startRendering();
-            selectAnimation(defaultFilename);
-        });
-        #else
-        FileCache.instance.initGraphics();
-        anim = new Animation(defaultFilename);
-        anim.preload();
-        #end
-*/
     }
 
     private function initMapHandler(map:MapBody) {
@@ -140,11 +109,6 @@ class Game {
                     if (topTile.x + MAP_SCREEN_TILED_WIDTH < map.width) topTile.x++;
             }
         }
-
-//        select = cast Browser.document.getElementById("def_select");
-//        select.onchange = onAnimChange;
-//        range = cast Browser.document.getElementById("groups_range");
-//        range.onchange = onGroupChange;
     }
 
     private function startRendering() {
@@ -159,7 +123,6 @@ class Game {
 
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             renderMap();
-//            renderAnim();
         }
 
         oldTimestamp = timestamp;
@@ -182,68 +145,6 @@ class Game {
 
         gameInfo.mh.drawTerrainRectNew(ctx, info);
     }
-
-    // animation rendering
-    private function selectAnimation(name:String) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        var now = Date.now().getTime();
-
-        trace('Show $name');
-        anim = new Animation(name);
-        anim.preload();
-
-        var currentTime = Date.now().getTime();
-        trace('$name is ready. Loading time: ${(currentTime - now)}ms');
-
-        initRange();
-    }
-
-    private function initRange() {
-        animationKeys = [];
-        for (key in anim.images.keys()) {
-            animationKeys.push(key);
-        }
-        untyped range.max = animationKeys.length;
-        untyped range.value = 0;
-        group = 0;
-        iFrame = 0;
-    }
-
-    private function initSelect(filenames:Array<String>) {
-        for (filename in filenames) {
-            var optionElement:OptionElement = cast Browser.document.createElement("Option");
-            optionElement.label = filename;
-            optionElement.value = filename;
-            select.options.add(optionElement);
-        }
-    }
-
-    private function onGroupChange() {
-        group = animationKeys[Std.parseInt(range.value)];
-        iFrame = 0;
-    }
-
-    private function onAnimChange() {
-        untyped selectAnimation(select.options[select.selectedIndex].value);
-    }
-
-    private function renderAnim() {
-        if (anim != null && anim.images.exists(group)) {
-            var image:IImage = anim.images[group][iFrame];
-            if(image != null) {
-                ctx.fillStyle = "blue";
-                ctx.fillRect(0, 0, canvas.width, canvas.height);
-                image.drawToPos(ctx, 0, 0, null);
-            }
-
-            iFrame++;
-            if (iFrame == anim.images[group].length) {
-                iFrame = 0;
-            }
-        }
-    }
-
     #end
 
 }
