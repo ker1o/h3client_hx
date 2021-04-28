d84b3f19b37026d5bfdf796576e8abd3efa8bf4e
Kirill Asyamolov <kirilla@playtika.com>
2021-04-28 10:13:43 +0300

    move blitting from SdlImage to blitter

--- src/client/maphandler/MapNormalBlitter.hx
+++ src/client/maphandler/MapNormalBlitter.hx
@@ -1,4 +1,6 @@
 package client.maphandler;
+import gui.animation.SdlImage;
+import js.html.ImageData;
 import js.html.CanvasRenderingContext2D;
 import gui.animation.IImage;
 import gui.geometries.Rect;
@@ -79,8 +81,50 @@ class MapNormalBlitter extends MapBlitter {
         //ToDo
     }
 
-    override function drawElement(source:IImage, sourceRect:Rect, destRect:Rect) {
-        source.drawToRect(context2D, destRect, sourceRect);
+    override function drawElement(source:IImage, src:Rect, dest:Rect) {
+        var dx1 = 0;
+        var dy1 = 0;
+        var dx2:Int = dest.w;
+        var dy2:Int = dest.h;
+        var dw = dx2 - dx1;
+        var dh = dy2 - dy1;
+        var px:Int = source.margins.x;
+        var py:Int = source.margins.y;
+
+        if (src != null) {
+            // count coordintaes related to the original image
+            dx1 = Std.int(Math.max(src.x - source.margins.x, 0));
+            dy1 = Std.int(Math.max(src.y - source.margins.y, 0));
+            dx2 = Std.int(Math.max(Math.min(src.x + src.w - source.margins.x, source.size.x), 0));
+            dy2 = Std.int(Math.max(Math.min(src.y + src.h - source.margins.y, source.size.y), 0));
+
+            // size of the printed area
+            dw = dx2 - dx1;
+            dh = dy2 - dy1;
+
+            // coorinates correction in case of margins
+            px = Std.int(Math.max(source.margins.x - src.x, 0));
+            py = Std.int(Math.max(source.margins.y - src.y, 0));
+        }
+
+        if (dw == 0 || dh == 0) return;
+        var surface = cast(source, SdlImage).surf;
+        var ctxData = this.context2D.getImageData(dest.x + px, dest.y + py, dw, dh).data;
+        var p = 0;
+        for (j in 0...dh) {
+            for (i in 0...dw) {
+                var pixelIndex = ((dy1 + j) * source.size.x + dx1 + i) * 4;
+                if (surface[pixelIndex + 3] == 255) {
+                    ctxData[p] = surface[pixelIndex];
+                    ctxData[p + 1] = surface[pixelIndex + 1];
+                    ctxData[p + 2] = surface[pixelIndex + 2];
+                    ctxData[p + 3] = surface[pixelIndex + 3];
+                }
+                p += 4;
+            }
+        }
+        var imgData = new ImageData(ctxData, dw, dh);
+        this.context2D.putImageData(imgData, dest.x + px, dest.y + py);
     }
 
     override function postProcessing() {
--- src/gui/animation/IImage.hx
+++ src/gui/animation/IImage.hx
@@ -1,10 +1,6 @@
 package gui.animation;
-
-
 import constants.id.PlayerColor;
-import gui.geometries.Rect;
 import gui.geometries.Point;
-import js.html.CanvasRenderingContext2D;
 
 typedef BufferType = js.lib.Uint8ClampedArray;
 
@@ -12,15 +8,11 @@ interface IImage {
     var fullsize:Point;
     var margins:Point;
     var size:Point;
-
     var width(get, set):Int;
     var height(get, set):Int;
 
     function horizontalFlip():Void;
     function verticalFlip():Void;
     function clone():IImage;
-
-    function drawToPos(where:CanvasRenderingContext2D, posX:Int, posY:Int, src:Rect, alpha:Int = 255):Void;
-    function drawToRect(where:CanvasRenderingContext2D, dest:Rect, src:Rect, alpha:Int = 255):Void;
     function setFlagColor(player:PlayerColor):Void;
 }
